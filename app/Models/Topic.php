8f85eedd5aa274100f1c2a2e6d17483f0c3be2e8
chunhuili <2947680759@qq.com>
2019-05-08 15:53:38 +0800

    add 第七章

--- app/Models/Topic.php
+++ app/Models/Topic.php
@@ -53,4 +53,9 @@ class Topic extends Model
     {
         return $this->hasMany(Reply::class);
     }
+
+    public function topReplies()
+    {
+        return $this->replies()->limit(5);
+    }
 }
--- app/Transformers/TopicTransformer.php
+++ app/Transformers/TopicTransformer.php
@@ -36,4 +36,9 @@ class TopicTransformer extends TransformerAbstract
     {
         return $this->item($topic->category, new CategoryTransformer());
     }
+
+    public function includeTopReplies(Topic $topic)
+    {
+        return $this->collection($topic->topReplies, new ReplyTransformer());
+    }
 }
